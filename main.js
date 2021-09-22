351c1877f74ee4040156707252259894d56fa76f
MatthisLM <matthislm@hotmail.com>
2021-09-22 16:39:44 +0200

    front

---
+++ main.js
@@ -0,0 +1,11 @@
+$( document ).ready(function() {
+   var settingsDiplayed = false
+   $('#settings > i').click(function() {
+    if (settingsDiplayed == true){
+        $('#settings > ul').hide()
+        settingsDiplayed = false;
+    } else {
+        $('#settings > ul').css("display", "inline-flex");
+        settingsDiplayed = true;
+    }
+})});
