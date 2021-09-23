026c04b282c921ca084859c33c4a75616ee5076a
MatthisLM <matthislm@hotmail.com>
2021-09-23 14:20:22 +0200

    theme foncé

--- main.js
+++ main.js
@@ -23,14 +23,3 @@ querySnapshot.forEach((doc) => {
     console.log(name, url)
 });
 
-$( document ).ready(function() {
-    var settingsDiplayed = false
-    $('#settings > i').click(function() {
-     if (settingsDiplayed == true){
-         $('#settings > ul').css("visibility", "hidden");
-         settingsDiplayed = false;
-     } else {
-         $('#settings > ul').css("visibility", "initial");
-         settingsDiplayed = true;
-     }
- })});
